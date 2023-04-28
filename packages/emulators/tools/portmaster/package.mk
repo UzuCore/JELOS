66e3bd1bde26061f543b6e10ea6fbde4b2503be5
byunjaeil <byunjaeil@gmail.com>
2023-04-28 15:33:45 +0900

    Update package.mk

--- packages/emulators/tools/portmaster/package.mk
+++ packages/emulators/tools/portmaster/package.mk
@@ -4,13 +4,11 @@ PKG_NAME="portmaster"
 PKG_LICENSE="CUSTOM"
 PKG_ARCH="any"
 PKG_URL="https://github.com/christianhaitian/PortMaster/raw/main/PortMaster.zip"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_SOURCE_DIR="PortMaster by christianhaitian"
-PKG_LONGDESC="PortMaster library"
+PKG_SHORTDESC="PortMaster"
+PKG_LONGDESC="PortMaster by christianhaitian"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  #mkdir -p ${INSTALL}/usr/share/themes/${PKG_NAME}
-  #cp -rf * ${INSTALL}/usr/share/themes/${PKG_NAME}
+  #test
 }
 
