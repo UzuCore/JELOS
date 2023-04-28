f0b6cfea576cb75c8cf59f58693b4ef227eb6614
byunjaeil <byunjaeil@gmail.com>
2023-04-29 05:33:52 +0900

    Update package.mk

--- packages/emulators/tools/portmaster/package.mk
+++ packages/emulators/tools/portmaster/package.mk
@@ -16,6 +16,6 @@ unpack() {
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/share/portmaster
-  cp -rf ${PKG_BUILD}/PortMaster ${INSTALL}/usr/share/portmaster
-  ln -sf /storage/roms/ports ${INSTALL}/usr/share/portmaster
+  cp -rf ${PKG_BUILD}/PortMaster/* ${INSTALL}/usr/share/portmaster
+  ln -sf ${INSTALL}/usr/share/portmaster ${INSTALL}/storage/roms/ports
 }
