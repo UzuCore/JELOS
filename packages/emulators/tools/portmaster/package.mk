bf7b9de81a23c170bd06aebe8308deefb5df440d
byunjaeil <byunjaeil@gmail.com>
2023-04-28 15:20:38 +0900

    Update package.mk

--- packages/emulators/tools/portmaster/package.mk
+++ packages/emulators/tools/portmaster/package.mk
@@ -1,10 +1,16 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+#UzuCore
 
 PKG_NAME="portmaster"
 PKG_LICENSE="CUSTOM"
-PKG_SITE="https://github.com/christianhaitian/PortMaster"
+PKG_ARCH="any"
 PKG_URL="https://github.com/christianhaitian/PortMaster/raw/main/PortMaster.zip"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SOURCE_DIR="PortMaster by christianhaitian"
 PKG_LONGDESC="PortMaster library"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  #mkdir -p ${INSTALL}/usr/share/themes/${PKG_NAME}
+  #cp -rf * ${INSTALL}/usr/share/themes/${PKG_NAME}
+}
+
