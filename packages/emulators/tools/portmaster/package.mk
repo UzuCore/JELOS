aedc6339485fa76fac8947d0c826bb954e4b7495
byunjaeil <byunjaeil@gmail.com>
2023-04-29 04:41:07 +0900

    Update package.mk

--- packages/emulators/tools/portmaster/package.mk
+++ packages/emulators/tools/portmaster/package.mk
@@ -7,3 +7,8 @@ PKG_URL="https://github.com/christianhaitian/PortMaster/raw/main/PortMaster.zip"
 PKG_SHORTDESC="PortMaster"
 PKG_LONGDESC="PortMaster by christianhaitian"
 PKG_TOOLCHAIN="manual"
+
+post_install() {
+  cp * ${INSTALL}/storage/portmaster
+  ln -s ${INSTALL}/storage/portmaster ${INSTALL}/storage/roms/ports
+}
