4d2fdefec95ad32716e8322efe9c4930fb678184
byunjaeil <byunjaeil@gmail.com>
2023-05-10 08:54:15 +0900

    remove

--- packages/virtual/gamesupport/package.mk
+++ packages/virtual/gamesupport/package.mk
@@ -8,7 +8,6 @@ PKG_SECTION="virtual"
 PKG_LONGDESC="Game support software metapackage."
 
 PKG_GAMESUPPORT="sixaxis gptokeyb jstest-sdl gamecontrollerdb sdljoytest control-gen"
-PKG_GAMESUPPORT+=" portmaster"
 
 PKG_DEPENDS_TARGET="${PKG_GAMESUPPORT}"
 
