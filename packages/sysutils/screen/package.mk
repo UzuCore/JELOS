aaa71a12420a20a5451bcec872e051cb4ae6d7f1
byunjaeil <byunjaeil@gmail.com>
2023-05-13 22:32:43 +0900

    restore

--- packages/sysutils/screen/package.mk
+++ packages/sysutils/screen/package.mk
@@ -6,7 +6,7 @@ PKG_VERSION="4.6.2"
 PKG_SHA256="1b6922520e6a0ce5e28768d620b0f640a6631397f95ccb043b70b91bb503fa3a"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/screen/"
-PKG_URL="http://ftp.kaist.ac.kr/gnu/screen/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_URL="http://ftpmirror.gnu.org/screen/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_LONGDESC="Screen is a window manager that multiplexes a physical terminal between several processes"
 
