76ce1324a83fed636c92d55853283e153758208e
byunjaeil <byunjaeil@gmail.com>
2024-03-05 12:14:52 +0900

    mirror2

--- packages/emulators/standalone/citra-sa/package.mk
+++ packages/emulators/standalone/citra-sa/package.mk
@@ -3,7 +3,7 @@
 
 PKG_NAME="citra-sa"
 PKG_LICENSE="MPLv2"
-PKG_SITE="https://github.com/UzuCore/citra-mirror"
+PKG_SITE="https://github.com/UzuCore/citra-sa"
 PKG_DEPENDS_TARGET="toolchain ffmpeg mesa SDL2 boost zlib libusb boost zstd control-gen"
 PKG_LONGDESC="Citra 3DS emulator"
 PKG_TOOLCHAIN="cmake"
